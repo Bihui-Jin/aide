94d0d9ff32a9127e660eed40fdd2cc7fb6ecaf04
Bihui Jin <virgil.b.jin@gmail.com>
2026-02-27 03:19:58 +0000

    update docker socket

--- aide/backend/backend_openai.py
+++ aide/backend/backend_openai.py
@@ -85,14 +85,14 @@ def get_docker_host_ip() -> str:
 def _setup_openai_client():
     global _client
     docker_host_ip = get_docker_host_ip()
-    a = subprocess.run(
-        ["curl", f"http://host.docker.internal:8000/v1/models"],
-        capture_output=True,
-        timeout=15,
-        text=True,
-    )
-    logger.info(f"curl rc={a.returncode}, out={a.stdout[:200]}, err={a.stderr[:200]}")
-    # logger.info(f"Resolved Docker host IP as: {docker_host_ip}")
+    # a = subprocess.run(
+    #     ["curl", f"http://host.docker.internal:8000/v1/models"],
+    #     capture_output=True,
+    #     timeout=15,
+    #     text=True,
+    # )
+    # logger.info(f"curl rc={a.returncode}, out={a.stdout[:200]}, err={a.stderr[:200]}")
+    # # logger.info(f"Resolved Docker host IP as: {docker_host_ip}")
     _client = openai.OpenAI(max_retries=0, base_url=f'http://host.docker.internal:8000/v1', api_key="testkey")
 
 
